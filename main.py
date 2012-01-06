5af7649e7b93040cd7f32eecf294edc4c6e0dd5c
Paul A. Sawaya <me@paulsawaya.com>
2012-01-05 23:35:10 -0500

    flask

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('main.html')
+    
+@app.route("/study/create")
+def main():
+    return render_template('study/create.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.logger.setLevel(logging.DEBUG)
+    app.config.update(DEBUG=True,PROPAGATE_EXCEPTIONS=True,TESTING=True)
+    app.run(debug=True,host='0.0.0.0',port=port)
