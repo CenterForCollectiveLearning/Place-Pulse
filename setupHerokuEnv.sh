90170bc259d04114a9895addd0d44bd3c452439d
Paul Sawaya <me@paulsawaya.com>
2012-01-10 03:54:37 -0500

    new url

--- setupHerokuEnv.sh
+++ setupHerokuEnv.sh
@@ -5,6 +5,6 @@ heroku config:add MONGO_USER="placepulse"
 heroku config:add MONGO_PASSWORD="Mr3D48wOe3H02D0"
 
 heroku config:add MONGO_URI="mongodb://placepulse:Mr3D48wOe3H02D0@ds029287.mongolab.com:29287/heroku_app2358184"
-heroku config:add PLACEPULSE_HOST="empty-robot-3398.herokuapp.com"
-heroku config:add PLACEPULSE_BASEURL="empty-robot-3398.herokuapp.com"
+heroku config:add PLACEPULSE_HOST="place-pulse-staging.herokuapp.com"
+heroku config:add PLACEPULSE_BASEURL="place-pulse-staging.herokuapp.com"
 
