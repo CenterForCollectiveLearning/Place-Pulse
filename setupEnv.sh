5a5309eb7619350b48a83f4aec44bc8e4516f8c6
Phil Salesses <salesses@mit.edu>
2012-02-09 23:28:53 -0500

    Changed sourceEnv.sh and added .less files for css

--- setupEnv.sh
+++ setupEnv.sh
@@ -3,4 +3,4 @@ export MONGO_PORT="27017"
 export MONGO_DBNAME="placepulse"
 export PLACEPULSE_HOST="localhost:8000"
 export PLACEPULSE_BASEURL="localhost:8000"
-source PlacePulse/bin/activate
+source bin/activate
