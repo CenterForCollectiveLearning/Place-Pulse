a738eadf0026238382e19b6cdc09fae04c7d6cc0
Paul Sawaya <me@paulsawaya.com>
2012-04-22 13:05:14 -0400

    add ProxyFixer

--- run_placepulse_tornado.py
+++ run_placepulse_tornado.py
@@ -3,6 +3,12 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from PlacePulse import app
 
+# ONLY RUN THIS BEHIND A PROXY!
+# Proxy Fixer needed for apache proxy in production, see http://flask.pocoo.org/docs/deploying/others/#proxy-setups
+
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(5000)
 IOLoop.instance().start()
