feab754393817c2363e2f18598358f6c80ee196f
Paul Sawaya <me@paulsawaya.com>
2012-04-21 21:44:17 -0400

    setupDeployEnv

---
+++ setupDeployEnv.sh
@@ -0,0 +1,8 @@
+export MONGO_HOSTNAME="localhost"
+export MONGO_PORT="27017"
+export MONGO_DBNAME="placepulse"
+export PLACEPULSE_HOST="pulse.media.mit.edu"
+export PLACEPULSE_BASEURL="pulse.media.mit.edu"
+export COOKIE_SECRET_KEY="a10ad1a40b754a4d9b663fac60556a78"
+export FACEBOOK_APP_ID="178485798938752"
+export FACEBOOK_APP_SECRET="3fc3c9f171bbb5221403307113662fe5"
