b4ba2bbe9cb0bc67a9ad61e0b4d03a82099c1719
Phil Salesses <phil.salesses@essess.com>
2012-08-16 19:05:05 +0200

    Converted Pages

--- PlacePulse/root.py
+++ PlacePulse/root.py
@@ -11,3 +11,15 @@ from db import Database
 @root.route("/about/")
 def load_about():
 	return auto_template('about.html')
+
+@root.route("/data/")
+def load_data():
+	return auto_template('data.html')
+
+@root.route("/faq/")
+def load_faq():
+	return auto_template('faq.html')
+
+@root.route("/contact/")
+def load_contact():
+	return auto_template('contact.html')
