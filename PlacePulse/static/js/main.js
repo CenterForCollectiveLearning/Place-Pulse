99ae1766c01c3b4e22cb7fe98e79450ff654e15c
Deepak Jagdish <djagdish@media.mit.edu>
2013-06-06 17:33:02 +0200

    front page revamped - larger streetview images; refactored html structure; score replaced by votes;

--- PlacePulse/static/js/main.js
+++ PlacePulse/static/js/main.js
@@ -6,10 +6,23 @@ var locs;
 var left_done = false;
 var right_done = false;
 
+$('#pano_right').hover(function() {$('#right_arrow').toggleClass('choice_button_hover')}, function() {$('#right_arrow').toggleClass('choice_button_hover')});
+
+$('#pano_left').hover(function() {$('#left_arrow').toggleClass('choice_button_hover')}, function() {$('#left_arrow').toggleClass('choice_button_hover')});
+
+$('#right_arrow').hover(function() {$('#pano_right').toggleClass('streetViewChoice_hover')}, function() {$('#pano_right').toggleClass('streetViewChoice_hover')});
+
+$('#left_arrow').hover(function() {$('#pano_left').toggleClass('streetViewChoice_hover')}, function() {$('#pano_left').toggleClass('streetViewChoice_hover')});
+
+$('#equal').hover(function() {$('#pano_left, #pano_right').toggleClass('streetViewChoice_hover')}, function() {$('#pano_left, #pano_right').toggleClass('streetViewChoice_hover')});
+
 function onStreetViewChoice(choice) {
     left_done = false;
     right_done = false;
     $("img.place").unbind('click').on("click", function() {void(0); });
+    $("#left_arrow").unbind('click').on("click", function() {void(0); });
+    $("#right_arrow").unbind('click').on("click", function() {void(0); });
+    $("#equal").unbind('click').on("click", function() {void(0); });
     $('.loadingmsg').css("display","block");
     $("ul.thumbnails").css("opacity", 0.2);
     $(".equalbutton").css("opacity", 0.2);
@@ -70,7 +83,10 @@ function getImagePair() {
             imagesLoaded( $('ul.thumbnails'), function(){
                 $("img.place").unbind('click');
                 $('#pano_left img.place').on("click", function() { onStreetViewChoice('left'); });
+                $('#left_arrow').on("click", function() { onStreetViewChoice('left'); });
                 $('#pano_right img.place').on("click", function() { onStreetViewChoice('right'); });
+                $('#right_arrow').on("click", function() { onStreetViewChoice('right'); });
+                $('#equal').on("click", function() { onStreetViewChoice('equal'); });
 
                 $('.loadingmsg').css("display","none");
                 $("ul.thumbnails").css("opacity", 1.0);
@@ -86,7 +102,8 @@ function getImagePair() {
 
 function getSVURL(lat, lng) {
     // TODO: re-add this SV-specific data: &fov=90&heading=235&pitch=10
-    return "http://maps.googleapis.com/maps/api/streetview?size=470x306&location=" + lat + "," + lng + "&sensor=false&key=AIzaSyABK8O6uR0xcb_GQDSum__7gVkJXsMKZWU";
+    //return "http://maps.googleapis.com/maps/api/streetview?size=470x306&location=" + lat + "," + lng + "&sensor=false&key=AIzaSyABK8O6uR0xcb_GQDSum__7gVkJXsMKZWU";
+    return "http://maps.googleapis.com/maps/api/streetview?size=470x306&location=" + lat + "," + lng + "&sensor=false";
 }
 
 $(document).ready(function() {
