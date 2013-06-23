457419cc755e14d4677bcba40cb0c9a2c1732b52
Daniel Smilkov <dsmilkov@gmail.com>
2013-06-23 11:25:03 -0400

    removed proxy lines for tornado web server

--- run_placepulse_tornado.py
+++ run_placepulse_tornado.py
@@ -2,13 +2,15 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from PlacePulse import app
+import os
 
 # ONLY RUN THIS BEHIND A PROXY!
 # Proxy Fixer needed for apache proxy in production, see http://flask.pocoo.org/docs/deploying/others/#proxy-setups
 
-from werkzeug.contrib.fixers import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app)
+#from werkzeug.contrib.fixers import ProxyFix
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+port = int(os.environ.get("PORT", 8000))
+http_server.listen(port)
 IOLoop.instance().start()
