0be2de4be94f3b7268510afd69215b73ea836fe5
Deepak Jagdish <djagdish@media.mit.edu>
2013-07-20 00:15:16 -0400

    modified to support launching multiple server instances on different ports, proxied via nginx on 80

--- run_placepulse_tornado.py
+++ run_placepulse_tornado.py
@@ -3,12 +3,7 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from PlacePulse import app
 import os
-
-# ONLY RUN THIS BEHIND A PROXY!
-# Proxy Fixer needed for apache proxy in production, see http://flask.pocoo.org/docs/deploying/others/#proxy-setups
-
-#from werkzeug.contrib.fixers import ProxyFix
-#app.wsgi_app = ProxyFix(app.wsgi_app)
+import sys
 
 is_maintenance_mode = False
  
@@ -19,10 +14,12 @@ def check_for_maintenance():
     if is_maintenance_mode:
         return 'Sorry, off for maintenance! We\'ll be back online in 10min', 503
 
-
-
+if(len(sys.argv)!=2):
+    print 'python run_placepulse_tornado.py #### \nwhere #### is port num'
+    sys.exit(0)
 
 http_server = HTTPServer(WSGIContainer(app))
-port = int(os.environ.get("PORT", 8000))
+port = int(sys.argv[1])
 http_server.listen(port)
+print 'Starting server instance on port: ', port
 IOLoop.instance().start()
