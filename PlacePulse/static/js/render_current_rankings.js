aad33b316df692cb67d7b5e5af95890ffd7ff3da
Deepak Jagdish <djagdish@media.mit.edu>
2013-07-21 17:38:38 -0400

    Normalized scores; Static row numbers added to rankings table;

--- PlacePulse/static/js/render_current_rankings.js
+++ PlacePulse/static/js/render_current_rankings.js
@@ -32,21 +32,34 @@ function getallstats(_studies) {
 
 function consolidate_results(_results, _studies) {
   var rows = {};
+
   for(var i=0;i<_results.length;i++) {
-    for(var j=0;j<_results[i].data.length;j++) {
+    var len = _results[i].data.length;
+    var min = _results[i].data[len-1]['trueskill']['score'];
+    var max = _results[i].data[0]['trueskill']['score'];
+    for(var j=0;j<len;j++) {
       if(i==0)
         rows[_results[i].data[j].place_name] = {};
       var this_study = _studies[_results[i].study];
-      rows[_results[i].data[j].place_name][this_study] = _results[i].data[j]['trueskill']['score'];
+      if(max!=min)
+        // normalizing scores between 1-10
+        rows[_results[i].data[j].place_name][this_study] = (((_results[i].data[j]['trueskill']['score'] - min) / (max-min))*9)+1;
+      else
+        rows[_results[i].data[j].place_name][this_study] = 1;
     }
   }
+  var k=1;
   for(var key in rows) {
     //construct a row
     var newrow = '<tr class="rankingrow"><td class="citycell">'+key+'</td><td class="scorecell">'+parseFloat((rows[key]["boring"]).toFixed(3))+'</td><'+
       'td class="scorecell">'+parseFloat((rows[key]["depressing"]).toFixed(3))+'</td><td class="scorecell">'+parseFloat((rows[key]["livelier"]).toFixed(3))+'</td><'+
       '<td class="scorecell">'+parseFloat((rows[key]["safer"]).toFixed(3))+'</td><td class="scorecell">'+parseFloat((rows[key]["wealthier"]).toFixed(3))+'</td></tr>';
     $('#rankings_table > tbody:last').append(newrow);
+
+    var newserial = '<tr><td class="numcell">'+k+'. </td></tr>';
+    $('#serialnum_table > tbody:last').append(newserial);
+    k++;
   }
 
-  $('#rankings_table').tablesorter({sortList: [[5,1]]});
+  $('#rankings_table').tablesorter({ sortList: [[5,1]] });
 }
